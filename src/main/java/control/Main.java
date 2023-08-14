9bd4fcb6486b1708b9b089f53c22b74936a90d71
PDSchmidt <pauldschmidtofficial@gmail.com>
2023-08-14 13:57:28 -0700

    Pom updated to produce a working JAR build of the project

--- src/main/java/control/Main.java
+++ src/main/java/control/Main.java
@@ -3,9 +3,19 @@ package control;
 import com.formdev.flatlaf.FlatDarkLaf;
 import view.LoginFrame;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        FlatDarkLaf.setup();
+        try {
+            FlatDarkLaf.setup();
+        } catch (Exception e) {
+            JFrame err = new JFrame();
+            err.setLocationRelativeTo(null);
+            err.setVisible(true);
+            err.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            JOptionPane.showMessageDialog(new JFrame(), "Error setting look and feel.");
+        }
         LoginFrame mainWindow = new LoginFrame("dark");
         mainWindow.setVisible(true);
     }
